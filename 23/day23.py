cb3ab2b3233e11756a76429cabdf7d318ce275c3
Jack Little <jack.little212@gmail.com>
2024-12-24 19:28:18 -0600

    Day23 Solution

--- 23/day23.py
+++ 23/day23.py
@@ -29,6 +29,16 @@ def main():
 
     print(f"Part 1: Size 3 cliques = {len(valid_cliques)}")
     print(f"Time: {time.time() - st}s\n")
+    st = time.time()
+
+    cliques = []
+    R = set()
+    X = set()
+    BronKerbosch(R, set(vertices.keys()), X, vertices, cliques)
+
+    maximum_clique = max(cliques,key=len)
+    print("Part 2: Password = ", ",".join(sorted(maximum_clique)))
+    print(f"Time: {time.time() - st}s\n")
 
 def is_clique(graph, clique) -> bool:
     for a in clique:
@@ -38,6 +48,35 @@ def is_clique(graph, clique) -> bool:
             if b not in graph[a]:
                 return False
     return True
+
+def BronKerbosch(R:set, P:set, X:set, graph, cliques) -> set:
+    # First iteration:
+    #  R is empty
+    #  P is list of vertices
+    #  X is empty
+
+    # if P and X are both empty then
+    #     report R as a maximal clique
+    # for each vertex v in P do
+    #     BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+    #     P := P \ {v}
+    #     X := X ⋃ {v})
+
+    # N(v) -> neighbors of v
+
+    if len(P) == 0 and len(X) == 0:
+        cliques.append(R)
+        return
+
+    for vertex in P.copy():
+        next_R = R | {vertex}
+        next_P = P & set(graph[vertex])
+        next_X = X & set(graph[vertex])
+
+        BronKerbosch( next_R, next_P, next_X, graph, cliques)
+
+        P.remove(vertex)
+        X.add(vertex)
 
 if __name__ == "__main__":
     main()
